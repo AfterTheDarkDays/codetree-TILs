07f22e41001c8ea116a7f566d6e1edc02f2401ea
Eunho Lee <afterthedarkdays@gmail.com>
2024-10-03 21:46:41 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241003/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.c
+++ 241003/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.c
@@ -1,43 +1,40 @@
 #include <stdio.h>
 
 int main() {
-    int dx[] = {1, -1, 0, 0};
+    int r, c;
+    int n, arr[100][100];
+    int dx[] = {-1, 1, 0, 0};
     int dy[] = {0, 0, -1, 1};
-
-    int n, r, c, found;
-    int grd[100][100];
 
     scanf("%d %d %d", &n, &r, &c);
 
-    for(int i = 0; i < n; i++)
-        for (int j = 0; j <n; j++)
-             scanf("%d", &grd[i][j]);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    r--; c--; 
 
-    r--;
-    c--;
-
-    printf("%d ", grd[r][c]);
-    while(1){
-        found = 0;
-
-        for(int i =0; i < 4; i++){
+    printf("%d ", arr[r][c]);
+    while (1) {
+        int next_x = -1, next_y = -1;
+        int max_value = arr[r][c]; 
+        for (int i = 0; i < 4; i++) {
             int nx = r + dx[i];
-            int ny= c + dy[i];
-
-            if((nx < 0 || nx >= n ) || (ny < 0 || ny >= n))
-                continue;
-            
-            if (grd[nx][ny] > grd[r][c]){
-                r = nx;
-                c = ny;
-                printf("%d ", grd[r][c]);
-
-                found = 1;
-                break;
+            int ny = c + dy[i];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                if (arr[nx][ny] > arr[r][c]) {
+                    next_x = nx;
+                    next_y = ny;
+                    break;
+                }
             }
         }
-        if (!found) break;
+        if (next_x == -1) 
+            break;
+        r = next_x;
+        c = next_y;
+        printf("%d ", arr[r][c]);
     }
-
     return 0;
 }
